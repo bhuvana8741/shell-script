dddecfb82393add3e869d8f8bd90c361710bdae4
bhuvana8741 <bhuvana33077@gmail.com>
2024-07-07 15:39:22 +0530

    added 3rd script

---
+++ 4-install-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0]
+then
+echo "Erorr :: please run this script with root access"
+else 
+echo "you are root user"
+fi
+yum install mysql -y
+ 
+ if[$? -ne 0]
+ then
+ echo"ERROR:: Installing mysql is failed"
+ exit1
+ else
+ echo"Installing mysql is success"
+ fi
